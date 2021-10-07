12ea6cd0d6badc5adac160c85f41c506d61703c3
botakveggie <83680830+botakveggie@users.noreply.github.com>
2021-10-07 14:05:58 +0800

    added profiles_api functionality

--- Profiles/ProfilesAPI.py
+++ Profiles/ProfilesAPI.py
@@ -5,3 +5,17 @@ from db import db
 sys.path.append("../")
 
 profiles_api = Blueprint("profiles", __name__)
+
+@profiles_api.route("/<int:userID>", methods = ["GET"])
+def retrieve_profile(userID):
+    profile = db[userID]
+    name = profile["name"]
+    scores = profile["scores"]
+
+    string1 = "Hello " + str(name) + "!\n\n" + "Your scores are:\n"
+    string2 = ""
+    for (score in scores):
+        string2 += str(score)
+        if score != scores[-1]:
+            string2 += ", "
+    return string1 + string2
