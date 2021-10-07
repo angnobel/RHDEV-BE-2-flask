9801e67d1597766aa2f8f0a1331e11efe6762525
botakveggie <83680830+botakveggie@users.noreply.github.com>
2021-10-07 17:12:45 +0800

    added get_min_score in profiles_api

--- Profiles/ProfilesAPI.py
+++ Profiles/ProfilesAPI.py
@@ -42,3 +42,25 @@ def delete_profile(userID):
     name = profile["name"]
     profile["name"] = "@"
     return "Sorry " + name + ", you've been thanosSnapped"
+
+@profiles_api.route("/<int:userID>/score", methods = ["GET"])
+def get_min_score(userID):
+    profile = db[userID]
+    score_list = profile["scores"]
+    
+    if profile["name"] == "@":
+        return "Sorry, profile does not exist."
+    if len(profile["scores"]) == 0:
+        return "You do not have any recorded results :("
+
+    min_score = request.args.get('minScore')
+    filtered_score_list = list(filter( lambda score:score >= min_score , score_list))
+    num_of_scores = len(filtered_score_list)
+    if num_of_scores == 0:
+        return "No scores found above " + str(min_score-1) + " :'("
+    string2 = "There are a total of " + str(num_of_scores) + ". \nHere they are!: \n"
+    for score in filtered_score_list:
+        string2 += str(score)
+        if score != filtered_score_list[-1]:
+            string2 += ", "
+    return string2
