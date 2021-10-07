7a98c66d7213e0598a2fbf68530025d2b81f47e6
botakveggie <83680830+botakveggie@users.noreply.github.com>
2021-10-07 16:15:35 +0800

    varioous changes

--- Profiles/ProfilesAPI.py
+++ Profiles/ProfilesAPI.py
@@ -13,18 +13,18 @@ def retrieve_profile(userID):
 
     if name == "@":
         return "Sorry, profile does not exist."
-    else:
-        scores = profile["scores"]
 
-        string1 = "Hello " + str(name) + "!\n\n"
-        string2 = "Your scores are:\n" + ""
-        for score in scores:
-            string2 += str(score)
-            if score != scores[-1]:
-                string2 += ", "
-        return string1 + string2
+    scores = profile["scores"]
 
-@profiles_api.route("/", methods = ["POST"])
+    string1 = "Hello " + str(name) + "!\n\n"
+    string2 = "Your scores are:\n" + ""
+    for score in scores:
+        string2 += str(score)
+        if score != scores[-1]:
+            string2 += ", "
+    return string1 + string2
+
+@profiles_api.route("/profiles", methods = ["POST"])
 def create_profile():
     content = request.get_json()
     content["scores"] = []
@@ -35,6 +35,10 @@ def create_profile():
 @profiles_api.route("/<int:userID>", methods = ["DELETE"])
 def delete_profile(userID):
     profile = db[userID]
+
+    if profile["name"] == "@":
+        return "Sorry, profile does not exist."
+    
     name = profile["name"]
     profile["name"] = "@"
     return "Sorry " + name + ", you've been thanosSnapped"
