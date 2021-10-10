3ae512c65cc2df1a8f28f737c6946742ff6a797a
Charles Lim <charleslimjh@u.nus.edu>
2021-10-11 04:34:18 +0800

    implemented AuthAPI, POST /auth/register and POST /auth/login

--- Auth/AuthAPI.py
+++ Auth/AuthAPI.py
@@ -1,7 +1,44 @@
 # Score API here
-from flask import Blueprint
+from flask import Blueprint, request, current_app, json
+from Auth.Auth_db import Auth_db
+import jwt
 import sys
-from db import db
 sys.path.append("../")
 
 auth_api = Blueprint("auth", __name__)
+
+
+@auth_api.route("/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+        req =  request.form
+
+        # checks for valid body
+        if "username" in req.keys() and "passwordHash" in req.keys():
+            Auth_db.append({"username": req["username"], "passwordHash": req["passwordHash"]})
+            return {"status": "success"}
+
+        return {"status": "error", "message": "invalid body"}
+
+@auth_api.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        req = request.form
+
+        # checks for valid body
+        if "username" in req.keys() and "passwordHash" in req.keys():
+            username = req["username"]
+            passwordHash = req["passwordHash"]
+
+            token = jwt.encode({'username': username,'passwordHash': passwordHash}, current_app.config['SECRET_KEY'], algorithm='HS256')
+            return json.jsonify({"status": "success", "token": token})
+                
+        return {"status": "error", "message": "invalid body"}
+
+
+
+# For testing, returns Auth_db
+# @auth_api.route("/", methods=["GET"])
+# def auth():
+#     if request.method == "GET":
+#         return {"status": "success", "auth_db": Auth_db}
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from Auth.AuthAPI import auth_api
 from Profiles.ProfilesAPI import profiles_api
 from flask import Flask
 from db import db
+from secret import SECRET_KEY
 
 
 # Write your flask code here
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = SECRET_KEY
 
 app.register_blueprint(profiles_api, url_prefix="/profiles")
 app.register_blueprint(auth_api, url_prefix="/auth")
