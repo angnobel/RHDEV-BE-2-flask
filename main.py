a7bd00840595e1843a533af819c50ccb5bc742cc
Charles Lim <charleslimjh@u.nus.edu>
2021-10-11 00:13:23 +0800

    implemented homepage in main.py

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ app = Flask(__name__)
 
 app.register_blueprint(profiles_api, url_prefix="/profiles")
 app.register_blueprint(auth_api, url_prefix="/auth")
+
+@app.route("/", methods=["GET"])
+def main():
+    return "Hello, and welcome!" 
+
+if __name__ == "__main__":
+    # Development
+    app.run("localhost", port=8080)
