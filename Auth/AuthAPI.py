6541f34002e725fc7b56b1335e19ac920dbadcda
davelowqx <lowqixiang@gmail.com>
2021-10-13 14:20:12 +0800

    first commit

--- Auth/AuthAPI.py
+++ Auth/AuthAPI.py
@@ -1,7 +1,60 @@
-# Score API here
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+import json
+import jwt
 import sys
-from db import db
 sys.path.append("../")
 
+secret = "asldfjlkasdj"
+#current_app.config['SECRET_KEY']
+
+def db_read(): 
+	return json.load(open("./users.json"))
+
+def db_contains(obj):
+	data = db_read()
+	return obj in data
+
+def db_findkey(username):
+	data = db_read()
+	for row in data:
+		if (row["username"] == id):
+			return row 
+	return None
+
+def db_insert(new_data): 
+	data = db_read()
+	if db_findkey(new_data["username"]):
+			raise ValueError("Profile already exists") 
+	data.append(new_data)
+	json.dump(data, open("./users.json", "w"))
+
 auth_api = Blueprint("auth", __name__)
+
+@auth_api.route("/login", methods=["POST"])
+def login():
+	token = request.args.get("token")
+	try:
+		payload = jwt.decode(token, secret, 
+				algorithms=["HS256"])
+		if (db_contains(payload)):
+			return jsonify({"error": None}), 200
+	except Exception as e:
+		print(str(e))
+
+	obj = request.get_json(force=True)
+	if db_contains(obj):
+		token = jwt.encode(obj, secret,
+				algorithm="HS256")
+		return jsonify({"error": None, "token" : token}), 200
+	return jsonify({"error": "incorrect user or password", "token": None}), 404 
+
+@auth_api.route("/register", methods=["POST"])
+def register():
+	data = request.get_json(force=True)
+	username = data["username"]
+	passwordHash = data["passwordHash"]
+	try:
+		db_insert({"username": username, "passwordHash": passwordHash})
+		return jsonify({"error": None}), 201
+	except ValueError as e:
+		return jsonify({"error": str(e)}), 403 
--- Profiles/ProfilesAPI.py
+++ Profiles/ProfilesAPI.py
@@ -1,7 +1,70 @@
-# Profile API here
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+import json
 import sys
-from db import db
 sys.path.append("../")
 
+def db_read(): 
+	return json.load(open("./profiles.json"))
+
+def db_findkey(name):
+	data = db_read()
+	for row in data:
+		if (row["name"] == name):
+			return row 
+	return None
+
+def db_insert(new_data): 
+	data = db_read()
+	if db_findkey(new_data["name"]):
+			raise ValueError("Profile already exists") 
+	data.append(new_data)
+	json.dump(data, open("./profiles.json", "w"))
+
+def db_delete(name):
+	data = db_read()
+	row = db_findkey(name)
+	if row:
+		data.remove(row)
+		json.dump(data, open("./profiles.json", "w"))
+	else: 
+		raise ValueError("Profile not found") 
+		
+
 profiles_api = Blueprint("profiles", __name__)
+
+# gets data of matching profile 
+@profiles_api.route("/<name>", methods=["GET"])
+def read(name):
+	row = db_findkey(name)
+	if row:
+		return jsonify({"error": None, "data" : row}), 200
+	else: 
+		return jsonify({"error": "not found", "data": None}), 404 
+
+# creates new profile
+@profiles_api.route("/<name>", methods=["POST"])
+def create(name):
+	try:
+		db_insert({"name": name, "scores":[]})
+		return jsonify({"error": None}), 201
+	except ValueError as e:
+		return jsonify({"error": str(e)}), 403 
+
+# deletes matching profile
+@profiles_api.route("/<name>", methods=["DELETE"])
+def delete(name):
+	try: 
+		db_delete(name)
+		return jsonify({"error": None}), 201
+	except ValueError as e:
+		return jsonify({"error": str(e)}), 403 
+
+# gets scores of matching profile, optionally filtered > minScore
+@profiles_api.route("/<name>/score", methods=["GET"])
+def read_minscore(name):
+	minScore = int(request.args.get("minScore") or 0)
+	row = db_findkey(name)
+	if row:
+		return jsonify({"error": None, "data" : list(filter(lambda x:x > minScore, row["scores"]))}), 200
+	else: 
+		return jsonify({"error": "not found", "data": None}), 404 
