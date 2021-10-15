0bba1a06733a46b48f087dd3e969df304f443014
botakveggie <83680830+botakveggie@users.noreply.github.com>
2021-10-16 01:15:35 +0800

    error messages for auth

--- Auth/AuthAPI.py
+++ Auth/AuthAPI.py
@@ -1,7 +1,62 @@
 # Score API here
-from flask import Blueprint
-import sys
+from flask import Blueprint, request
+import sys, jwt
 from db import db
 sys.path.append("../")
 
 auth_api = Blueprint("auth", __name__)
+
+@auth_api.route("/register", methods = ["POST"])
+def register():
+    content = request.get_json()
+    try:
+        # Check if username is provided
+        userName = content["userName"]
+    except KeyError:
+        return {"status": "fail", "message": "no userName"}
+    try:
+        # Check if password is provided
+        passwordHash = content["passwordHash"]
+    except KeyError:
+        return {"status": "fail", "message": "no password"}
+    
+    credentials.append(
+        {
+            "userName": userName,
+            "passwordHash": passwordHash
+        }
+    )
+    return {"status": "success", "message": "registered successfully"}
+
+@auth_api.route("/login", methods = ["POST"])
+def login():
+    content = request.get_json()
+    try:
+        # Check if username is provided
+        userName = content["userName"]
+    except KeyError:
+        return {"status": "fail", "message": "no userName"}
+    try:
+        # Check if password is provided
+        passwordHash = content["passwordHash"]
+    except KeyError:
+        return {"status": "fail", "message": "no password"}
+    
+    try:
+        #Check for username and password in my dictionary
+        exists = credentials[userName] == passwordHash
+    except KeyError:
+        return {"status": "fail", "message": "password is wrong"}
+    if exists:
+        token = jwt.encode({
+            "userName": userName,
+            "passwordHash": passwordHash
+        })
+        return {"status": "success", 
+        "message": {
+            "userName": userName,
+            "passwordHash": passwordHash,
+            "createdAt": 
+        }}
+    else:
+        return {"status": "fail", "message": "username is wrong"}
