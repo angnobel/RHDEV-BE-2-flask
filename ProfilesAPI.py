d3ad505f67c24f5fbb58a4dfed6a508b5bb1a2fe
Shawn <e0726012@u.nus.edu>
2021-10-20 10:37:20 +0800

    added create profile feature

--- ProfilesAPI.py
+++ ProfilesAPI.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import request
 from flask import Blueprint
-from flask import render_template
 from db import datab
 
 app = Flask(__name__)
@@ -27,43 +26,6 @@ def create_profile(name):
     datab.append(user_dict)
     return f"user {name} has been created"
 
-
-
-
-# @profile.route('/profiles/DELETE/<string:name>', methods=["DELETE"])
-@app.route('/profiles/DELETE/<string:name>', methods=["DELETE","GET"])
-def delete_profile(name):
-    wanted_profile_list = list(filter(lambda a: a["name"] != name, datab))
-    if len(datab) != len(wanted_profile_list):
-        return f"{name} successfully removed"
-    else:
-        return "name not in database"
-
-
-
-    # for obj in datab:
-    #     if obj["name"] == name:
-    #         datab.remove(obj)
-    #         return "success"
-    
-
-
-# @profile.route('/scores/GET/<string:name>/', methods=['GET' , 'POST'])
-@app.route('/scores/GET/<string:name>/', methods=['GET' , 'POST'])
-def get_above_minscore(name):
-    minscore = request.args.get('minScore',type=int,default=0)
-    score_list = []
-    for obj in datab:
-        if obj["name"] == str(name):
-            score_list = list(filter(lambda a : a > minscore, obj["scores"]))
-            score_dict = {"name":name, "scores": score_list}
-            return score_dict
-    if score_list == []:
-        return "candidate not found"
-
-@app.route('/test/',methods=['POST','GET'])
-def haha():
-    return render_template("test.html")
 
 
 if __name__ == "__main__":
