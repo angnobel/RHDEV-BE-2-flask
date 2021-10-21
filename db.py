9e31823375c9b6ed4e05c6705a4739192503cc69
Shawn <e0726012@u.nus.edu>
2021-10-21 17:19:06 +0800

    created database

---
+++ db.py
@@ -0,0 +1,12 @@
+datab = [{
+    "name": "Nobel",
+    "scores": [1, 2, 3, 4, 5]
+}, {
+    "name": "Richard",
+    "scores": [5, 4, 3, 2, 1]
+}, {
+    "name": "Hui Hui",
+    "scores": [9, 29, 34]
+}]
+
+profile_auth = []
