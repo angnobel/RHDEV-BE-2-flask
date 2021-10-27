ff51fbbba5bb6cf05e642451b9bc90d6865913f6
Trulydev50 <76103867+Trulydev50@users.noreply.github.com>
2021-10-27 21:55:24 +0800

    done

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from db import db
 # Write your flask code here
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = AUTH_SECRET_KEY
 app.register_blueprint(profiles_api, url_prefix="/profiles")
 app.register_blueprint(auth_api, url_prefix="/auth")
+
+@app.route("/", methods = ["GET"])
+def hello():
+    return "halo"
+if __name__== "main":
+    app.run("localhost", port=8080)
