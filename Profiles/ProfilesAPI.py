d39ed9de11df3182a8580a6d70727056aff65ed8
Trulydev50 <76103867+Trulydev50@users.noreply.github.com>
2021-10-27 22:38:57 +0800

    editted

--- Profiles/ProfilesAPI.py
+++ Profiles/ProfilesAPI.py
@@ -1,7 +1,56 @@
 # Profile API here
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 import sys
 from db import db
 sys.path.append("../")
 
-profiles_api = Blueprint("profiles", __name__)
+profiles_api = Blueprint("profiles", name)
+
+@profiles_api.route('/<int:id>/', methods = ["GET"])
+def retrieve(id):
+    if id > len(db)-1:
+        return jsonify({"Status" : "Error", "Message" : "Don't have"})
+
+    else:
+        dets = db[id]
+        return jsonify({"Status" : "Success", "Details" : dets})
+
+@profiles_api.route('/', methods = ["POST"])#wont work on googlechrome only on postman,check on postman
+def postid():
+    newprofile = {}
+    newprofname = request.form.get('name')
+    newprofile["name"] = newprofname
+
+    db.append(newprofile)
+
+    return jsonify({"Status" : "Success", "Details" : newprofile})
+
+
+@profiles_api.route('/<int:id>/', methods = ["DELETE"])
+def deleteid(id):
+    if id > len(db)-1:
+        return jsonify({"Status" : "Error", "Message" : "No such ID"})
+
+    else:
+        deleted = db[id]
+        del db[id]
+
+        return jsonify({"Status" : "Success", "New db" : db, "Deleted profile" : deleted})
+
+
+@profiles_api.route('/<int:id>/score', methods = ["GET"])
+def getabovemin(id):
+    if id > len(db)-1:
+        return jsonify({"Status" : "Error", "Message" : "No such ID"})
+
+    minscore = request.args.get('minScore')
+    scores = db[id]["scores"]
+
+    if minscore == '':
+        return jsonify({"Status" : "Success", "Details" : scores})
+    
+    abovemin = list(filter(lambda score: score > int(minscore), scores))#use assig
+    
+
+    return jsonify({"Status" : "Success", "Details" : abovemin})
+
