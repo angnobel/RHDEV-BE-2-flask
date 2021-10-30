fab475e5aa4d3b08a90c5ef52eea31270d417cd0
botakveggie <83680830+botakveggie@users.noreply.github.com>
2021-10-30 19:15:28 +0800

    login with token

--- Auth/AuthAPI.py
+++ Auth/AuthAPI.py
@@ -30,30 +30,40 @@ def register():
 
 @auth_api.route("/login", methods = ["POST"])
 def login():
-    content = request.get_json()
-    try:
-        # Check if username is provided
-        userName = content["userName"]
-    except KeyError:
-        return {"status": "fail", "message": "no userName"}
-    try:
-        # Check if password is provided
-        passwordHash = content["passwordHash"]
-    except KeyError:
-        return {"status": "fail", "message": "no password"}
-    
-    try:
-        #Check for username and password in credentials dictionary
-        exists = credentials[userName]["passwordHash"] == passwordHash
-    except KeyError:
-        return {"status": "fail", "message": "username or password is wrong"}
-    if exists:
-        token = jwt.encode({"userName": userName,"passwordHash": passwordHash}, 
-            current_app.config["AUTH_SECRET_KEY"], algorithm="HS256")
-        return json.jsonify({"status": "success", 
-        "token": token})
-    else:
-        return {"status": "fail", "message": "username is wrong"}
+    def check_details(jsonobject):
+        userName = jsonobject["userName"]
+        passwordHash = jsonobject["passwordHash"]
+        try:
+            #Check for username and password in credentials dictionary
+            exists = credentials[userName]["passwordHash"] == passwordHash
+        except KeyError:
+            return {"status": "fail", "message": "username or password is wrong"}
+        if exists:
+            token = jwt.encode({"userName": userName,"passwordHash": passwordHash}, 
+                current_app.config["AUTH_SECRET_KEY"], algorithm="HS256")
+            return json.jsonify({"status": "success", 
+            "token": token})
+        else:
+            return {"status": "fail", "message": "username is wrong"}
 
+    content = request.get_json()
+    token = request.args.get("token")
+    if token == None: #no token provided
+        try:
+            # Check if username is provided
+            userName = content["userName"]
+        except KeyError:
+            return {"status": "fail", "message": "no userName"}
+        try:
+            # Check if password is provided
+            passwordHash = content["passwordHash"]
+        except KeyError:
+            return {"status": "fail", "message": "no password"}
+        
+       return check_details(content)
+    else:
+        #token is provided, let's check the token
+        details = jwt.decode(token)
+        return check_details(details)
 
 
